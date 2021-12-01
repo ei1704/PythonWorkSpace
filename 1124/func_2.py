f4370eedde74e42642b3fafccb6869c7e72bda56
ei1704 <ryopey1231@gmail.com>
2021-12-01 10:41:32 +0900

    11/24

--- 1124/func_2.py
+++ 1124/func_2.py
@@ -1,15 +1,11 @@
 def tax_included(val, tax=10):
+    '''税込み金額を計算する。税率を省略した場合は10%になる'''
     num = "None"
     if val > 0 and tax > 0:
         num = val + val * (tax/100)
-        num = int(num)
-    if tax == 10:
-        ret = "{}の税込み金額は{}円".format(val, num)
-    else:
-        ret = "{}の消費税{}%の税込み金額は{}円".format(val, tax, num)
-    return ret
+    return num
 
 
-print(tax_included(5000))
-print(tax_included(5000, 8))
-print(tax_included(5000, -5))
+print("{}の税込み金額は{}円".format(5000, tax_included(5000)))
+print("{}の消費税{} % の税込み金額は{}円".format(5000, 8, tax_included(5000, 8)))
+print("{}の消費税{} % の税込み金額は{}円".format(5000, -5, tax_included(5000, -5)))
